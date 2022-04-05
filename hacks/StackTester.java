1db65167acd2972d35b6d364a759f88fb4a1042c
4disease <70781122+4disease@users.noreply.github.com>
2022-04-05 16:26:29 +0000

    AGGGGHHH!

---
+++ hacks/StackTester.java
@@ -0,0 +1,22 @@
+class StackTester {
+  
+    public static void main(String[] args)
+    {
+
+        Object[] queuestart = new Integer[] { 1, 2, 3};
+        Object[] stackfinal = new Integer[] {};
+        QueueManager startq = new QueueManager("Integers", queuestart);
+
+        startq.printQueue();
+        Stack finals = new Stack();
+        for (int i = 0; i < queuestart.length; i++) {
+            finals.push(queuestart[i]);
+        }
+        System.out.println("\nQueue to Stack:");
+        for (int i = 0; i < queuestart.length; i++) {
+            System.out.print(finals.pop() + " ");
+        }
+        System.out.println();
+
+    }
+}
